f61d7bd9ac9b6c6eaf30586ecac97d3df577fc13
AndrykVP <andriclucid@gmail.com>
2020-04-16 12:30:30 -0500

    Fix #1

--- commands/start.js
+++ commands/start.js
@@ -13,47 +13,43 @@ module.exports = {
         }
 
         // CHECK IF MEMBER HAS PERMISSIONS TO START A LOTTERY
-        if(message.member.roles.some(r => admins.includes(r.name)) || admins.includes(message.author.username)) {
-            
-            // ASSIGN VALUES TO SEND A MESSAGE BASED ON ARGUMENTS, OR TO DEFAULT
-            // IF NO ARGUMENTS ARE GIVEN
-            var hours, tickets, msg
-            if(args.length < 1) {
-                hours = 24
-                tickets = 1000
-                msg = 'We\'re starting a 24-hour Raffle'
-            }
-            else {
-                hours = args[0] ? args[0] : 24
-                tickets = args[1] ? args[1] : 1000
-                msg = args[2] ? args.slice(2).join(' ') : 'We\'re starting a ' + args[0] + '-hour Raffle'          
-            }
-    
-            // CALCULATES THE DRAW DATE BASED ON THE ARGUMENTS GIVEN
-            // OR DEFAULTS TO 24 HOURS FROM CURRENT DATE
-            var date = new Date()
-            date.setTime(date.getTime() +  (hours * 60 * 60 * 1000))
-    
-            // START LOTTERY SETTINGS AND PREVENT NEW LOTTERIES FROM BEING STARTED
-            store.set('drawdate', date.getTime())
-            store.set('maxTicket', tickets)
-            store.set('running', true)
-    
-            // CONSTRUCTS AND SENDS A RICHEMBED MESSAGE
-            const embed = new RichEmbed()
-                .setColor(color)
-                .setTitle('Starting a new Lottery!')
-                .setDescription(msg)
-                .addField('Draw Date', 'Winner will be drawn on ' + date, true)
-                .addField('Get A Number!', 'Use the command ' + prefix + 'number and type a number between 1 and ' + tickets.toLocaleString('en-US'), true)
-    
-            message.channel.send(embed)
-        }
-
-        else {
-            message.reply('Sorry, but only an admin can start a raffle')
+        if(!message.member.roles.some(r => admins.includes(r.name)) && !admins.includes(message.author.username)) {
+            message.reply('Sorry, but only an Admin can start a raffle')
             return
         }
+        
+        // ASSIGN VALUES TO SEND A MESSAGE BASED ON ARGUMENTS, OR TO DEFAULT
+        // IF NO ARGUMENTS ARE GIVEN
+        var hours, tickets, msg
+        if(args.length < 1) {
+            hours = 24
+            tickets = 1000
+            msg = 'We\'re starting a 24-hour Raffle'
+        }
+        else {
+            hours = args[0] ? args[0] : 24
+            tickets = args[1] ? args[1] : 1000
+            msg = args[2] ? args.slice(2).join(' ') : 'We\'re starting a ' + args[0] + '-hour Raffle'          
+        }
 
+        // CALCULATES THE DRAW DATE BASED ON THE ARGUMENTS GIVEN
+        // OR DEFAULTS TO 24 HOURS FROM CURRENT DATE
+        var date = new Date()
+        date.setTime(date.getTime() +  (hours * 60 * 60 * 1000))
+
+        // START LOTTERY SETTINGS AND PREVENT NEW LOTTERIES FROM BEING STARTED
+        store.set('drawdate', date.getTime())
+        store.set('maxTicket', tickets)
+        store.set('running', true)
+
+        // CONSTRUCTS AND SENDS A RICHEMBED MESSAGE
+        const embed = new RichEmbed()
+            .setColor(color)
+            .setTitle('Starting a new Lottery!')
+            .setDescription(msg)
+            .addField('Draw Date', 'Winner will be drawn on ' + date, true)
+            .addField('Get A Number!', 'Use the command ' + prefix + 'number and type a number between 1 and ' + tickets.toLocaleString('en-US'), true)
+
+        message.channel.send(embed)
 	}
 }
